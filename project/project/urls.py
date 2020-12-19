5d8d9fe3f0ada71bfa7085d797b6845155427829
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-19 23:21:15 +0100

    fixed url pattern in project for account

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,6 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('blog.urls')),
-    path("register/", include("account.urls")),
+    path("account/", include("account.urls")),
 ]
 
