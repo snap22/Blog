83c29fff6d72503a6e2a06f5971340933f5ca9c2
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-19 22:25:42 +0100

    url for new app

--- project/project/urls.py
+++ project/project/urls.py
@@ -18,8 +18,10 @@ from django.urls import path, include
 
 
 
+
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include('blog.urls')),
+    path("register/", include("account.urls")),
 ]
 
