97b9afaf181225272bd6e59a2b89f28e7b81400a
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-19 22:40:21 +0100

    Added new form to views.py

--- project/account/views.py
+++ project/account/views.py
@@ -1,14 +1,15 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+
+from .forms import AccountCreationForm
 
 def register(request):
     if request.method == "POST":
-        form = UserCreationForm(request.POST)
+        form = AccountCreationForm(request.POST)
         if form.is_valid():
             form.save()     # hodi uzivatela do databazy, a hashne heslo
             messages.succes(request, f"Account was successfully created.")
             return redirect("blog-home")
     else:
-        form = UserCreationForm()
+        form = AccountCreationForm()
     return render(request, "account/register.html", {"form": form})
