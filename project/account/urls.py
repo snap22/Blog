0969caaa396588d3d2eae8904415e40efe86a43d
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-19 23:21:01 +0100

    logout

--- project/account/urls.py
+++ project/account/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.register, name='account-register'),
+    path('register/', views.register, name='account-register'),
+    path('login/', views.login_view, name='account-login'),
+    path('logout/', views.logout_view, name='account-logout'),
 ]
--- project/account/views.py
+++ project/account/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
+from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
+
 
 from .forms import AccountCreationForm
 
+
+
 def register(request):
+    """ Registrácia užívateľa """
+    
     if request.method == "POST":
         form = AccountCreationForm(request.POST)
         if form.is_valid():
@@ -13,3 +20,20 @@ def register(request):
     else:
         form = AccountCreationForm()
     return render(request, "account/register.html", {"form": form})
+
+
+
+def login_view(request):
+    """ Prihlásenie užívateľa """
+
+    pass
+
+
+
+@login_required
+def logout_view(request):
+    """ Odhlásenie užívateľa """
+
+    logout(request)
+    messages.info(request, "You have been logged out.")
+    return redirect("blog-welcome")
