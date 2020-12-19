ee67f70d434c1ac56956ee088afa0bc47da037ed
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-19 23:12:35 +0100

    Welcome.html init

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
+    path('welcome/', views.welcome, name='blog-welcome'),
 ]
--- project/blog/views.py
+++ project/blog/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 
 # Create your views here.
 
 def home(request):
-    return render(request, "blog/home.html")
+    # Domovská stránka
+
+    if request.user.is_authenticated:
+        return render(request, "blog/home.html")
+    else:
+        return redirect("blog-welcome")
+
+def welcome(request):
+    # Uvodna stranka
+
+    return render(request, "blog/welcome.html")
