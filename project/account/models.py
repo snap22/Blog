1f6875bd68786f79809e8486910737e142cf92c5
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-26 19:09:27 +0100

    created profile model

--- project/account/models.py
+++ project/account/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Profile(models.Model):
+    """ Trieda, ktorá je rozšírením pre účet používateľa """
+
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    picture = models.ImageField(default="default.png", upload_to="profile_pictures")
+    
+
+    def __str__(self):
+        return f"Profile for { self.user.username }"
+
+
