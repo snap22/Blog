2c23800270f4a7b8465a77cc6796f3d768d235ae
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-26 21:27:52 +0100

    created profile view

--- project/account/views.py
+++ project/account/views.py
@@ -4,7 +4,7 @@ from django.contrib.auth import authenticate, logout, login
 from django.contrib.auth.decorators import login_required
 
 
-from .forms import AccountCreationForm
+from .forms import AccountCreationForm, AccountUpdateForm, ProfileUpdateForm
 
 
 
@@ -22,4 +22,32 @@ def register(request):
     else:
         form = AccountCreationForm()
     return render(request, "account/register.html", {"form": form})
+
+
+@login_required
+def profile(request):
+    """ Profil užívateľa """
+
+    if request.method == "POST":
+        user_form = AccountUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user.profile)
+
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(request, f"Your account has been updated.")
+            return redirect("account-profile")
+
+    else:
+        user_form = AccountUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+
+    context = {
+        "user_form" : user_form,
+        "profile_form" : profile_form,
+        "title" : request.user.username,
+    }
+
+    return render(request, "account/profile.html", context)
+
 
