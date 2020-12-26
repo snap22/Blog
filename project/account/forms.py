c6788a94f36979eeef369020e144eb7fefffd41b
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-26 21:27:08 +0100

    created account & profile update forms

--- project/account/forms.py
+++ project/account/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class AccountCreationForm(UserCreationForm):
@@ -13,3 +14,23 @@ class AccountCreationForm(UserCreationForm):
 
         model = User
         fields = ["username","email","password1","password2"]
+
+
+class AccountUpdateForm(forms.ModelForm):
+    """ Formulár pre aktualizáciu údajov o uzívateľovi """
+
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ["username","email"]
+
+
+
+
+class ProfileUpdateForm(forms.ModelForm):
+    """ Formulár pre aktualizáciu údajov o profile uzívateľa """
+
+    class Meta:
+        model = Profile
+        fields = ["picture"]
