bbc255c3f1cdda4627ceb083e1b1fa7eb03c0d29
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-26 21:27:31 +0100

    added overriden save method for profile model

--- project/account/models.py
+++ project/account/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from PIL import Image
 
 # Create your models here.
 
@@ -9,6 +10,17 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     picture = models.ImageField(default="default.png", upload_to="profile_pictures")
     
+
+    def save(self):
+        """ Override pre uloženie - uloží obrázok vo vhodnej forme """
+
+        super().save()
+        
+        image = Image.open(self.picture.path)
+        
+        if image.height > 300 or image.width > 300:
+            image.thumbnail((300,300))
+            image.save(self.picture.path)
 
     def __str__(self):
         return f"Profile for { self.user.username }"
