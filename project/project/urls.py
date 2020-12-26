02cc40f9ff17708fec6eb903252468ff33526c52
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-26 19:16:52 +0100

    added media to urls.py in DEBUG mode

--- project/project/urls.py
+++ project/project/urls.py
@@ -16,8 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +28,6 @@ urlpatterns = [
     path("logout/", auth_views.LogoutView.as_view(template_name="account/logout.html"), name="logout"),
 ]
 
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
