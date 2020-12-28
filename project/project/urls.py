158636d851834c3437fc9e0f1d44c6759a515939
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-28 17:50:28 +0100

    installed tinymce to django

--- project/project/urls.py
+++ project/project/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="account/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="account/logout.html"), name="logout"),
+    path('tinymce/', include('tinymce.urls')),
 ]
 
 
