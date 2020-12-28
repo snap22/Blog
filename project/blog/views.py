5211617ccbc8e84612f90f96f5b4d6646b78889b
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-28 20:08:21 +0100

    added post to views

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from .forms import PostCreationForm
 
 # Create your views here.
 
 def home(request):
-    # Domovská stránka
+    """ Domovská stránka"""
 
     if request.user.is_authenticated:
         return render(request, "blog/home.html")
@@ -12,6 +14,14 @@ def home(request):
         return redirect("blog-welcome")
 
 def welcome(request):
-    # Uvodna stranka
+    """ Úvodná stránka """
 
     return render(request, "blog/welcome.html")
+
+
+@login_required
+def post_new(request):
+    """ Tvorba nového príspevku """
+
+    form = PostCreationForm()
+    return render(request, "blog/posts/post_add.html", {"form": form})
