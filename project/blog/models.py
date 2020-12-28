0114c540e63f254c1a27f73fdeaa5b61b7abef71
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-28 21:04:50 +0100

    fixed date issues in models

--- project/blog/models.py
+++ project/blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from tinymce import models as tinymce_models
+from django.http import request
 
 
 
@@ -10,11 +11,12 @@ class Post(models.Model):
 
     title = models.CharField(max_length = 100)
     content = tinymce_models.HTMLField()
-    date = models.DateTimeField(timezone.now)
+    date = models.DateTimeField(default=timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Post: [{self.title}] by {self.author.username}"
+
     
 
 
@@ -24,7 +26,7 @@ class Comment(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     content = models.TextField()
-    date = models.DateTimeField(timezone.now)
+    date = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return f"Comment to: [{self.post.title}] by {self.author.username}"
