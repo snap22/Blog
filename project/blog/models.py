dbe6baac1d5b42912c1049cea5998eec7d3bfe77
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-28 17:50:45 +0100

    Updated post & created comment

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from tinymce.models import HTMLField
 
 # Create your models here.
 
@@ -8,8 +9,21 @@ class Post(models.Model):
     """ Trieda predstavujúca model pre príspevok """
 
     title = models.CharField(max_length = 100)
-    content = models.TextField()
+    content = HTMLField()
     date = models.DateTimeField(timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-        
+
+    def __str__(self):
+        return f"Post: [{self.title}] by {self.author.username}"
     
+
+class Comment(models.Model):
+    """ Trieda predstavujúca model pre komentár k danému príspevku """
+
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    content = models.TextField()
+    date = models.DateTimeField(timezone.now)
+
+    def __str__(self):
+        return f"Comment to: [{self.post.title}] by {self.author.username}"
