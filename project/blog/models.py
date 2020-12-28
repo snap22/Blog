a4eea65b0ea9d90efcc8b9637f1ddbec105a7105
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-28 20:08:05 +0100

    changed content to tinymce field

--- project/blog/models.py
+++ project/blog/models.py
@@ -1,21 +1,22 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from tinymce.models import HTMLField
+from tinymce import models as tinymce_models
 
-# Create your models here.
+
 
 class Post(models.Model):
     """ Trieda predstavujúca model pre príspevok """
 
     title = models.CharField(max_length = 100)
-    content = HTMLField()
+    content = tinymce_models.HTMLField()
     date = models.DateTimeField(timezone.now)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"Post: [{self.title}] by {self.author.username}"
     
+
 
 class Comment(models.Model):
     """ Trieda predstavujúca model pre komentár k danému príspevku """
