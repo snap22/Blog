72dd5ae64cdd26e189d78cc52f5bc8b7ea50e670
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-29 22:52:03 +0100

    updated view for post_view

--- project/blog/views.py
+++ project/blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect, get_object_or_404
 from django.http import HttpResponse
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import PostCreationForm
+from .forms import PostCreationForm, CommentCreationForm
 from .models import Post, Comment
 
 # Create your views here.
@@ -39,12 +39,31 @@ def post_new(request):
     return render(request, "blog/posts/post_add.html", {"form": form})
 
 
+
 def post_view(request, post_id):
     """ Zobrazenie konkrétneho príspevku """
     
     found_post = get_object_or_404(Post, pk=post_id)
+    comments = Comment.objects.filter(post=found_post).order_by("-date")
+
+    #formulár pre komentár
+    if request.method == "POST":
+        comment_form = CommentCreationForm(request.POST)
+        if comment_form.is_valid():
+            created_comment = comment_form.save(commit=False)
+            created_comment.author = request.user
+            created_comment.post = found_post
+            created_comment.save()
+
+            return redirect(request.path)
+    else:
+        comment_form = CommentCreationForm()
+
     context = {
         "post": found_post,
+        "comments": comments,
+        "form": comment_form,
+        "comments_count": comments.count(),
     }
 
     return render(request, "blog/posts/post_view.html", context)
