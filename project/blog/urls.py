264919b50a241de776052efd11242065578beb1e
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-29 20:11:02 +0100

    added post_view to urls

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.home, name='blog-home'),
     path('welcome/', views.welcome, name='blog-welcome'),
     path('post/new/', views.post_new, name='blog-post-new'),
+    path('post/<int:post_id>/', views.post_view, name='blog-post-view'),
 ]
