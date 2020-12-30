7c3ab7d9905a8446eb518f0f575d45976035b3eb
snap22 <39875321+snap22@users.noreply.github.com>
2020-12-30 21:48:50 +0100

    added url for UpdateView

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('', views.home, name='blog-home'),
     path('welcome/', views.welcome, name='blog-welcome'),
     path('post/new/', views.post_new, name='blog-post-new'),
-    path('post/<int:post_id>/', views.post_view, name='blog-post-view'),
+    path('post/<int:pk>/', views.post_view, name='blog-post-view'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='blog-post-edit'),
 ]
