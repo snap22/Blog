14efef797bee9d411c1a4aa5e58d05fbf468f389
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-08 22:53:18 +0100

    added a pre_save signal for deleting old image

--- project/account/signals.py
+++ project/account/signals.py
@@ -1,7 +1,7 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Profile
+from .models import Profile, User
 
 
 @receiver(post_save, sender=User)
@@ -15,4 +15,23 @@ def create_profile(sender, instance, created, **kwargs):
 @receiver(post_save, sender=User)
 def save_profile(sender, instance, **kwargs):
     """ Metóda, ktorá uloží zmeny v profile """
+
     instance.profile.save()
+
+
+@receiver(pre_save, sender=User)
+def pre_save_profile(sender, instance, **kwargs):
+    """ Metóda, ktorá vymaže nie predvolenú starú fotku pred uložením """
+
+    # ak objekt sa len vytvara tak jeho ID je None
+    if instance.id is None:
+        return None
+
+    old_picture = User.objects.get(id=instance.id).profile.picture
+    if old_picture.name == "default.png":
+        return None
+
+    new_picture = instance.profile.picture
+    if (old_picture.name != new_picture.name):
+        old_picture.delete(save=False)
+    
