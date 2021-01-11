790d975cf30dd83104613a2eb05bd1271c8ef06e
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-11 21:04:40 +0100

    added pass change to urls

--- project/account/urls.py
+++ project/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('register/', views.register, name='account-register'),
     path("profile/", views.profile, name="account-profile"),
     path("profile/inspect/<int:pk>/", views.profile_inspect, name="account-profile-inspect"),
+    path("profile/password/", views.change_password, name="account-password-change"),
 ]
