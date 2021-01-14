219a57ab317081b922ffeba153121916bea8e69f
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-14 23:34:07 +0100

    changed welcome & home urls

--- project/blog/urls.py
+++ project/blog/urls.py
@@ -2,13 +2,13 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
-    path('welcome/', views.welcome, name='blog-welcome'),
+    path('', views.welcome, name='blog-welcome'),
+    path('home/', views.home, name='blog-home'),
+    path('browse/', views.browse_posts, name='blog-browse'),
     path('post/new/', views.post_new, name='blog-post-new'),
     path('post/<int:pk>/', views.post_view, name='blog-post-view'),
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='blog-post-edit'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='blog-post-delete'),
     path('post/comment/<int:pk>/edit/', views.CommentUpdateView.as_view(), name='blog-comment-edit'),
     path('post/comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='blog-comment-delete'),
-    path('browse/', views.browse_posts, name='blog-browse'),
 ]
