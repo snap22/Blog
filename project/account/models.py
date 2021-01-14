617275d45881d6ea2bfb25d7317b8953dd355440
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-14 21:08:44 +0100

    created a safe method for get picture & save error catch

--- project/account/models.py
+++ project/account/models.py
@@ -1,26 +1,38 @@
 from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
+from project.settings_custom import DEFAULT_PICTURE, DEFAULT_PICTURE_URL, PROFILE_PICTURES_LOCATION
+import os
 
 
 class Profile(models.Model):
     """ Trieda, ktorá je rozšírením pre účet používateľa """
 
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    picture = models.ImageField(default="default.png", upload_to="profile_pictures")
+    picture = models.ImageField(default=DEFAULT_PICTURE, upload_to=PROFILE_PICTURES_LOCATION)
     info = models.TextField(default="No info was provided", blank=True, null=True)
     
+    def get_picture(self):
+        """ Metóda pre bezpečné získanie obrázka používateľa """
+
+        if os.path.exists(self.picture.path):
+            return self.picture.url
+        return DEFAULT_PICTURE_URL
 
     def save(self, *args, **kwargs):
         """ Override pre uloženie - uloží obrázok vo vhodnej forme """
 
         super().save(*args, **kwargs)
-        
-        image = Image.open(self.picture.path)
-        
-        if image.height > 300 or image.width > 300:
-            image.thumbnail((300,300))
-            image.save(self.picture.path)
+
+        try:
+            image = Image.open(self.picture.path)
+            
+            if image.height > 300 or image.width > 300:
+                image.thumbnail((300,300))
+                image.save(self.picture.path)
+
+        except FileNotFoundError:   
+            pass
 
     def __str__(self):
         return f"Profile for { self.user.username }"
