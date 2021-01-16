9cf1132351ddc5bbee11de0c9afa09295b182811
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-16 21:11:24 +0100

    imported views to init

--- project/blog/views/__init__.py
+++ project/blog/views/__init__.py
@@ -1,3 +1,3 @@
-from .main_views import home, welcome, browse_posts
+from .main_views import home, welcome, browse_posts, about, contacts
 from .post_views import post_new, post_view, PostUpdateView, PostDeleteView
 from .comment_views import CommentUpdateView, CommentDeleteView
