783e4834d99c2f79117c2c24d00dc07748cbd1e2
snap22 <39875321+snap22@users.noreply.github.com>
2021-01-20 20:58:50 +0100

    removed comment

--- project/blog/admin.py
+++ project/blog/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import Post, Comment, ContactMessage
 
-# Registrácia modelu aby ho bolo vidno v /admin 
 admin.site.register(Post)
 admin.site.register(Comment)
 admin.site.register(ContactMessage)
