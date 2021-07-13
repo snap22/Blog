431b218f6278a94add53bd02045bd3c831d89c7e
snap22 <39875321+snap22@users.noreply.github.com>
2021-07-13 21:31:53 +0200

    reset password urls & templates

--- project/project/urls.py
+++ project/project/urls.py
@@ -27,6 +27,10 @@ urlpatterns = [
     path("account/", include("account.urls")),
     path("login/", auth_views.LoginView.as_view(template_name="account/login.html", redirect_authenticated_user=True), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="account/logout.html"), name="logout"),
+    path("reset_password/", auth_views.PasswordResetView.as_view(template_name="account/password_reset/password_reset.html"), name="reset_password"),
+    path("reset_password_done/", auth_views.PasswordResetDoneView.as_view(template_name="account/password_reset/password_reset_done.html"), name="password_reset_done"),
+    path("reset_password/<uidb64>/<token>", auth_views.PasswordResetConfirmView.as_view(template_name="account/password_reset/password_reset_confirm.html"), name="password_reset_confirm"),
+    path("reset_password_complete/", auth_views.PasswordResetCompleteView.as_view(template_name="account/password_reset/password_reset_complete.html"), name="password_reset_complete"),
     path("tinymce/", include('tinymce.urls')),
 ]
 
