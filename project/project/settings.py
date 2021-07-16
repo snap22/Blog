1c27dc8f4a81175884bdfe749c278e2a901d3145
snap22 <39875321+snap22@users.noreply.github.com>
2021-07-16 12:15:08 +0200

    updated static root

--- project/project/settings.py
+++ project/project/settings.py
@@ -126,18 +126,17 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR,"staticfiles")
 STATIC_URL = "/static/"
 
-MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 
+MEDIA_ROOT = os.path.join(BASE_DIR,"media")
 MEDIA_URL = "/media/"
 
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "blog-home"  # redirect ak sa uspesne prihlasim
-
 LOGIN_URL = "login"               # redirect ak je stranka login_required a uzivatel nie je prihlaseny
-
 LOGOUT_REDIRECT_URL = "blog-welcome"    #redirect ak sa uzivatel odhlasi
 
 # Pre email
@@ -159,7 +158,6 @@ AWS_S3_FILE_OWERWRITE = False   # nezmeni subor ak ma rovnaky nazov ako uz je v 
 AWS_DEFAULT_ACL = None  
 
 AWS_S3_REGION_NAME = "eu-central-1"
-
 AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
