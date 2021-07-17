e60575503de843f00b31f2512c9c0494b8105b3b
snap22 <39875321+snap22@users.noreply.github.com>
2021-07-17 14:59:39 +0200

    added error_views to blog.views

--- project/blog/views/__init__.py
+++ project/blog/views/__init__.py
@@ -1,3 +1,4 @@
 from .main_views import home, welcome, browse_posts, about, contacts
 from .post_views import post_new, post_view, PostUpdateView, PostDeleteView
 from .comment_views import CommentUpdateView, CommentDeleteView
+from .error_views import error_400, error_403, error_404, error_500
